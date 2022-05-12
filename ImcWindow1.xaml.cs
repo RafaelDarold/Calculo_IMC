3e6a449651d96cab9e9a2534fadf0cbc84a70268
rafae <rafae@R_ANGELODAROLD>
2022-05-12 19:17:53 -0400

    Adicionar arquivos de projeto.

---
+++ ImcWindow1.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Calculo_IMC
+{
+    /// <summary>
+    /// Lógica interna para ImcWindow1.xaml
+    /// </summary>
+    public partial class ImcWindow1 : Window
+    {
+        public ImcWindow1()
+        {
+            InitializeComponent();
+            Loaded += ImcWindow_Loaded; ;
+        }
+
+        private void ImcWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Txt_peso.Focus();
+        }
+
+
+        private void Txt_altura_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                Txt_altura.Focus();
+        }
+
+
+        private void bt_calcular_click(object sender, RoutedEventArgs e)
+        {
+            double peso, altura, resultado;
+            peso = Convert.ToDouble(Txt_peso.Text);
+            altura = Convert.ToDouble(Txt_altura.Text);
+
+            resultado = peso / (altura * altura);
+            Txt_imc.Text = resultado.ToString();
+        }
+
+        private void bt_sair_imc_click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void bt_limpar_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.Key == Key.Enter)
+                Txt_peso.Focus();
+        }
+
+        private void bt_limpar_Click(object sender, RoutedEventArgs e)
+        {
+            Txt_peso.Clear();
+            Txt_altura.Clear();
+            Txt_imc.Clear();
+
+            Txt_peso.Focus();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculo_IMC
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            txt_login.Focus();
+        }
+
+        private void txt_login_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void bt_entrar_Click(object sender, RoutedEventArgs e)
+        {
+            string senha = pw_senha.Password.ToString();
+            string login = txt_login.Text;
+
+            if (login == "R.Darold" && senha == "27261010")
+            {
+                MessageBox.Show("usuario logado");
+                ImcWindow1 Imc = new ImcWindow1();
+                Imc.Show();
+                this.Close();
+            }
+            else
+                MessageBox.Show("usuario desconhecido");
+        }
+
+        private void bt_sair_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txt_login_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                pw_senha.Focus();
+            }
+        }
+    }
+}
